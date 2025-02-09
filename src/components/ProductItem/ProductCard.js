5b97f6d5160a5757e692735450cdc4fe97284dc0
ducbinhnguyennd <ducbinhnguyennd@gmail.com>
2025-02-09 18:14:52 +0700

    update cn 92

--- src/components/ProductItem/ProductCard.js
+++ src/components/ProductItem/ProductCard.js
@@ -17,8 +17,8 @@ const ProductCard = ({ sanpham }) => {
       <img className="product-image" src={sanpham.image} alt={sanpham.name} />
       <div className="name-sp">{sanpham.name}</div>
       <div className="original-price">
-      <div className="price">{sanpham.price} đ</div>
-       <span className="old-price">{sanpham.price} đ</span>
+      <div className="price">{sanpham.price}₫</div>
+       <span className="old-price">{sanpham.price}₫</span>
       </div>
 
       
