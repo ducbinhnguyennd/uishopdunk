69ba7ec4390b1b7d4732352047e0b572eeb9dc76
trafdual <traz08102003@gmail.com>
2025-02-17 12:14:38 +0700

    f

--- src/Layout/Admin/DangNhapLayout/DangNhapLayout.js
+++ src/Layout/Admin/DangNhapLayout/DangNhapLayout.js
@@ -2,11 +2,66 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './DangNhapLayout.scss'
 import { useState } from 'react'
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import {ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function DagNhapLayout () {
   const [ispassword, setispassword] = useState(true)
+  const [password, setpassword] = useState('')
+  const [username, setusername] = useState('')
+
+  const validate = () => {
+    let isValid = true
+    if (!password) {
+      toast.error('bạn chưa nhập mật khẩu', {
+        position: 'top-right',
+        autoClose: 2000 // Ẩn sau 2 giây
+      })
+      isValid = false
+    }
+    if (!username) {
+      toast.error('bạn chưa nhập tài khoản', {
+        position: 'top-right',
+        autoClose: 2000 // Ẩn sau 2 giây
+      })
+      isValid = false
+    }
+
+    return isValid
+  }
+
+  const handleLogin = async () => {
+    try {
+      if (validate()) {
+        const response = await fetch('http://localhost:3005/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            username,
+            password
+          })
+        })
+        const data = await response.json()
+        if (data.message) {
+          toast.error(data.message, {
+            position: 'top-right',
+            autoClose: 2000 // Ẩn sau 2 giây
+          })
+        } else {
+          localStorage.setItem('data', data)
+          window.location.href = '/admin'
+        }
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className='login_container'>
+      <ToastContainer />
       <div className='login_main'>
         <div className='login_left'>
           <video autoPlay={true} muted loop playsInline>
@@ -22,7 +77,15 @@ function DagNhapLayout () {
           <div className='login_input'>
             <label htmlFor=''>Tài khoản</label>
             <div className='divinput_login'>
-              <input type='text' placeholder='Nhập tài khoản' />
+              <input
+                type='text'
+                placeholder='Nhập tài khoản'
+                value={username}
+                onChange={e => {
+                  setusername(e.target.value)
+                }}
+                autoComplete='off'
+              />
             </div>
           </div>
           <div className='login_input'>
@@ -31,6 +94,11 @@ function DagNhapLayout () {
               <input
                 type={ispassword ? 'password' : 'text'}
                 placeholder='Nhập mật khẩu'
+                value={password}
+                onChange={e => {
+                  setpassword(e.target.value)
+                }}
+                autoComplete='new-password'
               />
               <FontAwesomeIcon
                 icon={ispassword ? faEyeSlash : faEye}
@@ -39,7 +107,7 @@ function DagNhapLayout () {
             </div>
           </div>
           <div className='login_button'>
-            <button>Đăng nhập</button>
+            <button onClick={handleLogin}>Đăng nhập</button>
           </div>
         </div>
       </div>
